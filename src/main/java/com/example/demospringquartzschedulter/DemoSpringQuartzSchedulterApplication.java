694d536ba0422f5e116690cfa6d443e234ef1cbd
huytv7 <huytv7@msb.com.vn>
2024-03-18 15:35:11 +0700

    init

---
+++ src/main/java/com/example/demospringquartzschedulter/DemoSpringQuartzSchedulterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringquartzschedulter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringQuartzSchedulterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSpringQuartzSchedulterApplication.class, args);
+  }
+
+}
